7f37ab40cf0c35e732a23206937d82bbed121521
Reynanwq <reynanwq@gmail.com>
2023-07-22 23:56:18 -0300

    projeto spring boot

---
+++ genshin-impact/src/main/java/com/api/genshinimpact/GenshinImpactApplication.java
@@ -0,0 +1,13 @@
+package com.api.genshinimpact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinImpactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenshinImpactApplication.class, args);
+	}
+
+}
