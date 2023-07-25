a7c42f5c648d5519e8f067d7a3ce02058f8f9b75
Reynanwq <reynanwq@gmail.com>
2023-07-25 17:11:50 -0300

    Characters insert in database

--- genshin-impact/src/main/java/com/api/genshinimpact/service/CrudCharactersService.java
+++ genshin-impact/src/main/java/com/api/genshinimpact/service/CrudCharactersService.java
@@ -1,7 +1,6 @@
 package com.api.genshinimpact.service;
 
 import com.api.genshinimpact.arm.Characters;
-import com.api.genshinimpact.arm.Updates;
 import com.api.genshinimpact.repository.CharactersRepository;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +12,85 @@ public class CrudCharactersService {
         this.charactersRepository = charactersRepository;
     }
     public void insertData(){
-        Characters characters0 = new Characters(0, "", "", 0, "", "", "", "", "", "");
-        this.charactersRepository.save(characters0);
+        Characters albedo = new Characters(1, "Albedo", "Cavaleiros de Favonius", 90, "Um gênio conhecido como Príncipe do Giz, ele é o Chefe Alquimista e Capitão da Equipe de Investigação dos Cavaleiros de Favonius.", "12/2020", "Masculino", "Ataque Normal: Técnica da Espada de Favonius - Branco | Abiogênese - Radiação Solar | Ritual da Progenitura - Maré Tectônica", "Pressão Calcária: Fase de Ascensão 1 | Sabedoria em Uma Garrafa: Fase de Ascensão 4 | Gênio Despertado: Passive", "Flor de Éden | Éon da Vida | Esplendor Solar | Queda Divina | Maré de Hadeano | Peoira da Purificação");
+        Characters Alhaitham = new Characters(2, "", "", 0, "", "", "", "", "", "");
+        Characters Aloy = new Characters(3, "", "", 0, "", "", "", "", "", "");
+        Characters Amber = new Characters(4, "", "", 0, "", "", "", "", "", "");
+        Characters Arataki_Itto = new Characters(5, "", "", 0, "", "", "", "", "", "");
+        Characters Baizhu = new Characters(6, "", "", 0, "", "", "", "", "", "");
+        Characters Barbara = new Characters(7, "", "", 0, "", "", "", "", "", "");
+        Characters Beidou = new Characters(8, "", "", 0, "", "", "", "", "", "");
+        Characters Bennett = new Characters(9, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(10, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(11, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(12, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(13, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(14, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(15, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(16, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(17, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(18, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(19, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(20, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(21, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(22, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(23, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(24, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(25, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(26, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(27, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(28, "", "", 0, "", "", "", "", "", "");
+        Characters Alhaitham = new Characters(29, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(30, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(31, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(32, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(33, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(34, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(35, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(36, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(37, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(38, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(39, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(40, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(41, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(42, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(43, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(44, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(45, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(46, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(47, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(48, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(49, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(50, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(51, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(52, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(53, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(54, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(55, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(56, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(57, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(58, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(59, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(60, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(61, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(62, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(63, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(64, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(65, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(66, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(67, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(68, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(69, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(70, "", "", 0, "", "", "", "", "", "");
+        Characters albedo = new Characters(71, "", "", 0, "", "", "", "", "", "");
+
+        this.charactersRepository.save(albedo);
+        System.out.println("Characters save in database");
     }
 
     //----------- SELECIONAR DADOS DA TABELA CHARACTERS ------
     public void selectData(){
-        Iterable<Characters> characters = this.charactersRepository.findAll();
+        Iterable<Character> characters = this.charactersRepository.findAll();
         for (Characters characters1 : characters){
             System.out.println(characters1);
         }
