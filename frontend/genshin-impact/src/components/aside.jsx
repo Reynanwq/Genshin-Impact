c26bc2da4a42d9a2190edb1bec698a9da748b5af
Reynanwq <reynanwq@gmail.com>
2023-08-08 13:02:18 -0300

    development aside

--- frontend/genshin-impact/src/components/aside.jsx
+++ frontend/genshin-impact/src/components/aside.jsx
@@ -4,15 +4,49 @@ import './aside.css'; // Certifique-se de ter o arquivo Aside.css no mesmo diret
 const Aside = () => {
   return (
     <aside className="aside">
-      <h2>Related Links</h2>
+      <h1>Popular Pages</h1>
       <ul>
-        <li><a href="#">IMAGE</a></li>
-        <li><a href="#">IMAGE</a></li>
-        <li><a href="#">IMAGE</a></li>
+        <li><a href="https://genshin.hoyoverse.com/pc-launcher/?utm_source=GS-BR-PC-X-search-keywords-classic-20230128&mhy_trace_channel=ga_channel&new=1&gclid=Cj0KCQjwz8emBhDrARIsANNJjS5n1p53ICYQO3NE3VKqZX6C2EBBZiDdfE1rwFHDdxV58yXQx04vxv0aAssHEALw_wcB#/" target="_blank" rel="noopener noreferrer">Download</a></li>
+
+        <li><a href="https://www.youtube.com/playlist?list=PL28_eRFIuaoIzVhtU0pqTow-HV62IMREa" target="_blank" rel="noopener noreferrer">Gameplay Playlist</a></li>
+
+        <li><a href="https://www.twitch.tv/directory/game/Genshin%20Impact" target="_blank" rel="noopener noreferrer">Twitch Gameplay</a></li>
+
+        <h2>Streamers on Twitch</h2>
+
+        <li><a href="https://www.twitch.tv/zajef77" target="_blank" rel="noopener noreferrer">Zajef77</a></li>
+
+        <li><a href="https://www.twitch.tv/eullls" target="_blank" rel="noopener noreferrer">Eullls</a></li>
+
+        <li><a href="https://www.twitch.tv/schnapsidee_" target="_blank" rel="noopener noreferrer">schnapsidee</a></li>
+
+        <li><a href="https://www.twitch.tv/markin" target="_blank" rel="noopener noreferrer">Markin</a></li>
+
+        <li><a href="https://www.twitch.tv/tocadoscoelhos" target="_blank" rel="noopener noreferrer">Toca dos Coelhos</a></li>
+
+        <li><a href="https://www.twitch.tv/barbraba" target="_blank" rel="noopener noreferrer">Barbraba</a></li>
+
+        <li><a href="https://www.twitch.tv/nekkopii" target="_blank" rel="noopener noreferrer">Nekkopii</a></li>
+
+        <li><a href="https://www.twitch.tv/sharkh3art" target="_blank" rel="noopener noreferrer">SharkH3art</a></li>
+
+        <li><a href="https://www.twitch.tv/ekkonmia" target="_blank" rel="noopener noreferrer">Ekkonmia</a></li>
+
+        <li><a href="https://www.twitch.tv/cardigangod" target="_blank" rel="noopener noreferrer">CardiganGod</a></li>
+
+        <li><a href="https://www.twitch.tv/kinjoleef" target="_blank" rel="noopener noreferrer">kinjoleef</a></li>
+
+        <li><a href="https://www.twitch.tv/keikianne" target="_blank" rel="noopener noreferrer">keikianne</a></li>
+
+        <li><a href="https://www.twitch.tv/xanjos" target="_blank" rel="noopener noreferrer">Xanjos</a></li>
+
+        <li><a href="https://www.twitch.tv/ahjamos" target="_blank" rel="noopener noreferrer">ahjamos</a></li>
+
+        <li><a href="https://www.twitch.tv/dunkin_sailor" target="_blank" rel="noopener noreferrer">Dunkin_Sailor</a></li>
+
       </ul>
     </aside>
   );
 };
 
-export default Aside;
-  
+export default Aside;  
