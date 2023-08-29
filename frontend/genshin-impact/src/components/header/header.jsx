9d4095cd0de766cd6a441ac21acab7b3b148b802
Reynanwq <reynanwq@gmail.com>
2023-08-29 12:22:39 -0300

    :test_tube: test add a image in the header

--- frontend/genshin-impact/src/components/header/header.jsx
+++ frontend/genshin-impact/src/components/header/header.jsx
@@ -4,9 +4,13 @@ const Header = () => {
   return(
     <header className="header-principal">
       <h1>Genshin Impact</h1>
+      {/* <img
+        src="../../assets/home/image_from_homepage.jpg"
+        alt="image from Home Page"
+        className="image_from_home_header"
+      /> */}
     </header>
   )
-
 };
 
 export default Header;
