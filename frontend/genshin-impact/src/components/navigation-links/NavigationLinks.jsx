468daa72c65614580bedfb8c0f98bcc82737d286
Reynanwq <reynanwq@gmail.com>
2023-08-29 11:08:12 -0300

    :bug: fix: fixing error regarding div

--- frontend/genshin-impact/src/components/navigation-links/NavigationLinks.jsx
+++ frontend/genshin-impact/src/components/navigation-links/NavigationLinks.jsx
@@ -3,51 +3,51 @@ import './navigation-links.css';
 const NavigationLinks = () => {
   return (
     <nav className="navigation-links">
-      <Link to='/achievements' className="no-underline">
         <div className="nav_box">
+        <Link to='/achievements' className="no-underline">
         <h3>Achievements</h3>
+        </Link>
         </div>
-      </Link>
+      <div className="nav_box">
       <Link to="/characters" className="no-underline">
-      <div className="nav_box">
       <h3>Character</h3>
+      </Link> 
       </div>
-      </Link>
+      <div className="nav_box">
       <Link to={"/fishing"} className="no-underline">
-      <div className="nav_box">
       <h3>Fishing</h3>
-      </div>
       </Link>
+      </div>
+      <div className="nav_box">
       <Link to={"/food"} className="no-underline">
-      <div className="nav_box">
       <h3>Food</h3>
-      </div>
       </Link>
+      </div>
+      <div className="nav_box">
       <Link to={"/potions"} className="no-underline">
-      <div className="nav_box">
       <h3>Potions</h3>
-      </div>
       </Link>
+      </div>
+      <div className="nav_box">
       <Link to={"/quests"} className="no-underline">
-      <div className="nav_box">
       <h3>Quests</h3>
-      </div>
       </Link>
+      </div>
+      <div className="nav_box">
       <Link to={"/statisticCharacter"} className="no-underline">
-      <div className="nav_box">
       <h3>Statistic about Character</h3>
-      </div>
       </Link>
+      </div>
+      <div className="nav_box">
       <Link to={"/updates"} className="no-underline">
-      <div className="nav_box">
       <h3>Updates</h3>
-      </div>
       </Link>
-      <Link to={"/weapons"} className="no-underline">
+      </div>
       <div className="nav_box">
+      <Link to={"/weapons"} className="no-underline">
       <h3>Weapons</h3>
-      </div>
       </Link>
+      </div>
     </nav>
   );
 };
