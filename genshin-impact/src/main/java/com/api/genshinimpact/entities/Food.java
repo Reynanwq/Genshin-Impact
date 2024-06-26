79bd98a11af5312cdd046b24f229d7abb165942d
Reynan Paiva <90296084+Reynanwq@users.noreply.github.com>
2024-06-26 00:33:18 -0300

    Update Food.java

--- genshin-impact/src/main/java/com/api/genshinimpact/entities/Food.java
+++ genshin-impact/src/main/java/com/api/genshinimpact/entities/Food.java
@@ -5,32 +5,32 @@ import jakarta.persistence.*;
 import java.io.Serializable;
 
 @Entity
-public class Food implements Serializable {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "food_type")
+public abstract class Food implements Serializable {
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @Column(nullable = false)
     private String food_name;
+
     @Column(nullable = false)
     private String food_description;
+
     @Column(nullable = false)
     private Integer rarity;
-    @Column(nullable = false)
-    private String food_type;
-    @Column(nullable = false)
-    private String url;
 
-    @Deprecated
-    public Food(){};
+    public Food() {
+    }
 
-    public Food(Integer id, String food_name, String food_description, Integer rarity, String food_type, String url) {
+    public Food(Integer id, String food_name, String food_description, Integer rarity) {
         this.id = id;
         this.food_name = food_name;
         this.food_description = food_description;
         this.rarity = rarity;
-        this.food_type = food_type;
-        this.url = url;
     }
 
     public Integer getId() {
@@ -65,31 +65,6 @@ public class Food implements Serializable {
         this.rarity = rarity;
     }
 
-    public String getFood_type() {
-        return food_type;
-    }
-
-    public void setFood_type(String food_type) {
-        this.food_type = food_type;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    @Override
-    public String toString() {
-        return "Food{" +
-                "id=" + id +
-                ", food_name='" + food_name + '\'' +
-                ", food_description='" + food_description + '\'' +
-                ", rarity=" + rarity +
-                ", food_type='" + food_type + '\'' +
-                ", url='" + url + '\'' +
-                '}';
-    }
+    // Método abstrato a ser implementado nas subclasses
+    public abstract String getFoodType();
 }
